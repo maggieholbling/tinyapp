5393c622369d7a82660dba7b8f9e96ba2239fc91
Maggie <maggieholbl@gmail.com>
2019-09-03 18:58:04 +0000

    refactoring

--- generateRandomString.js
+++ generateRandomString.js
@@ -2,10 +2,10 @@ const generateRandomString = function () {
   let result = "";
   const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const upperNumbers = [];
+
   for(let i = 0; i < characters.length; i++) {
     
     const upperChar = characters[i].toUpperCase();
-    
     upperNumbers.push(upperChar);
     
     if (i < 10) {
@@ -18,7 +18,8 @@ const generateRandomString = function () {
   for(let k = 0; k < 6; k++) {
     result += newArray[Math.floor(Math.random() * newArray.length)];
   }
+
   return result;
-}
+};
 
 module.exports = generateRandomString;
