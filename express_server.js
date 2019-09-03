c5842500aab408a61963439c4ed0107f297cf0eb
Maggie <maggieholbl@gmail.com>
2019-09-03 02:28:19 +0000

    added a re-direct function for final short links

--- express_server.js
+++ express_server.js
@@ -13,8 +13,6 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-generateRandomString();
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,10 +23,12 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  if (!urlDatabase[shortURL]) {
+    urlDatabase[shortURL] = req.body.longURL;
+  }
+  res.redirect("/urls/" + shortURL);
 });
-//req.body.longURL
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -41,12 +41,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
