32b546c101654d7bf99fafb3108b8b59b348ef84
Maggie <maggieholbl@gmail.com>
2019-09-03 00:04:13 +0000

    added a views page

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,6 +11,11 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let urlData = { urls: urlDatabase };
+  res.render("urls_index", urlData);
 });
 
 app.get("/urls.json", (req, res) => {
