c46641dfa8178ba1426166929a94b26a81c1e709
Maggie <maggieholbl@gmail.com>
2019-09-04 19:56:59 +0000

    added error handlers for registration

--- express_server.js
+++ express_server.js
@@ -12,8 +12,57 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {};
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+/**
+ * Loops through an object and tests the element's (nested object's) - key value against the given value.
+ * If the value is found, it returns an object with emailExists: true and the user (nested) object it tested against.
+ * If not found, it returns an object with emailExists: false
+ * @param {String} userKey the user's (nested object's) key being tested for the given value
+ * @param {Object} object the object to loop through
+ * @param {*} value the value that's being looked for - must be primitive type
+ */
+const userKeyLookup = (userKey, object, value) => {
+  Object.keys(object).forEach((key) => {
+    if (object[key][userKey] === value) {
+      return {emailExists: true, user: object[key]};
+    }
+  })
+  return {emailExists: false};
+};
+
+//registration page
+app.get('/register', (req, res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("registration", templateVars);
+});
+
+//handling registration
+app.post('/register', (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    res.status(400);
+    res.send("Please fill out your email and password.");
+  
+  } else if (userKeyLookup("email", users, req.body.email).emailExists) {
+      res.status(400);
+      res.send("Email already in use.");
+  
+  } else {
+    
+    let userRandomID = generateRandomString(9);
+    users[userRandomID] = {
+      id: userRandomID, 
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    res.cookie("user_id", userRandomID);
+    res.redirect("/urls");
+  }
+});
 
 //login
 app.post('/login', (req, res) => {
@@ -30,7 +79,7 @@ app.post('/logout', (req, res) => {
 //page shows index of urls
 app.get("/urls", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase
   };                   
   res.render("urls_index", templateVars);
@@ -38,7 +87,7 @@ app.get("/urls", (req, res) => {
 
 //generating a short url
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
+  let shortURL = generateRandomString(6);
   if (!urlDatabase[shortURL]) {
     urlDatabase[shortURL] = req.body.longURL;
   } else {
@@ -49,14 +98,14 @@ app.post("/urls", (req, res) => {
 
 //new url page
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 //page shows short url
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     longURL: urlDatabase[req.params.shortURL],
     shortURL: req.params.shortURL };
   res.render("urls_show", templateVars);
@@ -79,7 +128,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //redirecting back to original url
 app.get("/u/:shortURL", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL, templateVars);
 });
@@ -89,7 +138,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.redirect("/urls/", templateVars);
 });
 
