fd5a74e4c1d50b2cd0dd3464c398beb4c04de24d
Maggie <maggieholbl@gmail.com>
2019-09-05 02:06:41 +0000

    only users who created links can delete and update them and checking if randomIDs are truly unique

--- express_server.js
+++ express_server.js
@@ -2,14 +2,14 @@ const express = require("express");
 var cookieParser = require('cookie-parser');
 const generateRandomString = require("./generateRandomString");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW333" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW333" }
 };
 
 const users = {};
@@ -35,6 +35,22 @@ const userKeyLookup = (userKey, object, value) => {
   return result;
 };
 
+/**
+ * Loops through an object and trims it down to just the nested objects, whose key (specified by param innerKey) value matches the given value.
+ * @param {String} innerKey the user's (nested object's) key being tested for the given value
+ * @param {Object} object the object to loop through
+ * @param {*} value the value that's being looked for - must be primitive type
+ */
+const filterByInnerKey = (innerKey, object, value) => {
+  let result = {};
+  Object.keys(object).forEach((key) => {
+    if (object[key][innerKey] === value) {
+      result[key] = object[key];
+    }
+  })
+  return result;
+};
+
 //registration page
 app.get('/register', (req, res) => {
   let templateVars = { user: users[req.cookies["user_id"]] };
@@ -52,8 +68,10 @@ app.post('/register', (req, res) => {
       res.send("Email already in use.");
   
   } else {
-    
-    let userRandomID = generateRandomString(9);
+    let userRandomID;
+    do { userRandomID = generateRandomString(9);
+    } while (users[userRandomID]);
+
     users[userRandomID] = {
       id: userRandomID, 
       email: req.body.email,
@@ -105,35 +123,46 @@ app.post('/logout', (req, res) => {
 
 //page shows index of urls
 app.get("/urls", (req, res) => {
+  if (!req.cookies["user_id"]) res.redirect("/login");
+  const userURLDatabase = filterByInnerKey("userID", urlDatabase, req.cookies["user_id"]);
   let templateVars = {
     user: users[req.cookies["user_id"]],
-    urls: urlDatabase
+    urls: userURLDatabase
   };                   
   res.render("urls_index", templateVars);
 });
 
 //generating a short url
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString(6);
-  if (!urlDatabase[shortURL]) {
-    urlDatabase[shortURL] = req.body.longURL;
-  } else {
-    res.send("Error, please refresh."); //possibility for recursion/ while loop to find a unique string
-  }
-  res.redirect("/urls/" + shortURL);
+  let shortURL;
+  do { shortURL = generateRandomString(6);
+  } while (urlDatabase[shortURL]);
+  // if (urlDatabase[shortURL]) {
+  //   res.send("Error, please refresh."); //possibility for recursion/ while loop to find a unique string
+  // } else {
+    urlDatabase[shortURL] = {
+      longURL: req.body.longURL,
+      userID: req.cookies["user_id"]
+    };
+    res.redirect("/urls/" + shortURL);
+  // }
 });
 
 //new url page
 app.get("/urls/new", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    res.redirect("/login");
+  } else {
   let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
+  }
 });
 
 //page shows short url
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
     user: users[req.cookies["user_id"]],
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     shortURL: req.params.shortURL };
   res.render("urls_show", templateVars);
 });
@@ -141,23 +170,28 @@ app.get("/urls/:shortURL", (req, res) => {
 //updating links
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  urlDatabase[req.params.shortURL] = req.body.newLongURL;
+  if (urlDatabase[shortURL].userID !== req.cookies["user_id"]) {
+    res.redirect("/urls");
+  } else {
+  urlDatabase[req.params.shortURL].longURL = req.body.newLongURL;
   res.redirect("/urls/" + shortURL);
+  }
 })
 
 
 //deleting a url
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
+  if (urlDatabase[shortURL].userID === req.cookies["user_id"]) {
+    delete urlDatabase[shortURL];
+  }
   res.redirect("/urls/");
 });
 
 //redirecting back to original url
 app.get("/u/:shortURL", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL, templateVars);
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -165,8 +199,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
-  res.redirect("/urls/", templateVars);
+  res.redirect("/urls/");
 });
 
 app.listen(PORT, () => {
